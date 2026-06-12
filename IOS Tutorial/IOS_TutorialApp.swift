f99c336683c6aefe0000307b77622a47d6636b15
NandanaKumaraMA <nandanakumarama@gmail.com>
2026-06-12 18:24:59 +0530

    first_commit

---
+++ IOS Tutorial/IOS_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_TutorialApp.swift
+//  IOS Tutorial
+//
+//  Created by student3 on 2026-06-06.
+//
+
+import SwiftUI
+
+@main
+struct IOS_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
