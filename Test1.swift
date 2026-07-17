f1041d883e7f5efe5ec10ddf99d96ac4db591cb4
NandanaKumaraMA <nandanakumarama@gmail.com>
2026-07-17 17:14:41 +0530

    deleted_test -files

--- Test1.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Test1.swift
-//  IOS Tutorial
-//
-//  Created by student3 on 2026-07-04.
-//
-
--- Views/Tabs/MainTabView.swift
+++ Views/Tabs/MainTabView.swift
@@ -1,6 +1,7 @@
 
 import SwiftUI
 
+
 // The root shell for the app. Each tab owns its own NavigationStack
 // (declared inside HomeTab / StatsTab / MapTab / SettingsTab already),
 // so pushing a game from Home doesn't affect the other tabs.
